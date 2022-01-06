08c8813be96e54c2e45b88935ebdff9a0ab5907f
NoAuthor <83094641+CodeHubDC@users.noreply.github.com>
2022-01-06 12:34:30 +0100

    Added Input Field Focus Effect

--- src/scripts/currency-calculator.js
+++ src/scripts/currency-calculator.js
@@ -7,3 +7,19 @@ calculateButton.onclick = function(event) {
     calculateButton.style.display = "none";
     resultPopup.style.display = "initial";
 }
+
+/* UI */
+var InputBoxes = document.getElementsByClassName("input-box");
+var InputFields = document.getElementsByTagName("input");
+
+for (var i = 0; i < InputFields.length; i++) {
+    const pass = i;
+    InputFields[pass].addEventListener("focus", (event) => {
+        InputBoxes[pass].style.border = "1px solid var(--primary-color)";
+        InputBoxes[pass].style.boxShadow = "rgba(0, 0, 0, 0) 0px 0px 0px";
+    });
+    InputFields[pass].addEventListener("focusout", (event) => {
+        InputBoxes[pass].style.border = "";
+        InputBoxes[pass].style.boxShadow = "";
+    });
+}
